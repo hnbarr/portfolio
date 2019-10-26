5e032379e85910ad31387ae876f51ce0b5922417
Heather Barr <hnbarr@yahoo.com>
2019-10-26 11:18:31 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div id='header' className='component'>
+                <h6>test header</h6>
+            </div>
+        )
+    }
+}
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div id='main' className='component'>
+               <h6>test main</h6> 
+            </div>
+        )
+    }
+}
