4bacdc38ec664b71cf9fc712d6a34b2014ba7ed9
Heather Barr <hnbarr@yahoo.com>
2019-10-28 19:19:50 -0500

    updates

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -6,25 +6,25 @@ export default class AppPreview extends Component {
             <div id='appPreviewLayout'>
                 <h6 id='standupTitle'>standup</h6>
                 <ul id='gallery'>
-                    <li id="p1" class="flipper">
-                        <div class="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
-                        <div class="back">
+                    <li id="p1" className="flipper">
+                        <div className="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
+                        <div className="back">
                             <h2>Glyph Profiles</h2>
-                            <p class="author">by <a href="http://dribbble.com/brandclay">Sean Farrell</a></p>
+                            <p className="author">by <a href="http://google.com">Google Link?</a></p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                             <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p class="date">Feb 18, 2013</p>
+                            <p className="date">Feb 18, 2013</p>
                         </div>
                     </li>
 
-                    <li id="p1" class="flipper">
-                        <div class="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
-                        <div class="back">
+                    <li id="p1" className="flipper">
+                        <div className="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
+                        <div className="back">
                             <h2>Glyph Profiles</h2>
-                            <p class="author">by <a href="http://dribbble.com/brandclay">Sean Farrell</a></p>
+                            <p className="author">by <a href="http://google.com">Google Link?</a></p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                             <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p class="date">Feb 18, 2013</p>
+                            <p className="date">Feb 18, 2013</p>
                         </div>
                     </li>
 
@@ -32,7 +32,7 @@ export default class AppPreview extends Component {
                         <div class="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
                         <div class="back">
                             <h2>Glyph Profiles</h2>
-                            <p class="author">by <a href="http://dribbble.com/brandclay">Sean Farrell</a></p>
+                            <p class="author">by <a href="http://google.com">Google Link?</a></p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                             <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                             <p class="date">Feb 18, 2013</p>
