053088a06f7de1fade77baee9d26ac30fbcd17d9
Heather Barr <hnbarr@yahoo.com>
2019-10-29 07:11:39 -0500

    updates to image component

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -6,40 +6,31 @@ export default class AppPreview extends Component {
             <div id='appPreviewLayout'>
                 <h6 id='standupTitle'>standup</h6>
                 <ul id='gallery'>
-                    <li id="p1" className="flipper">
-                        <div className="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
-                        <div className="back">
-                            <h2>Glyph Profiles</h2>
-                            <p className="author">by <a href="http://google.com">Google Link?</a></p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p className="date">Feb 18, 2013</p>
-                        </div>
-                    </li>
-
-                    <li id="p1" className="flipper">
-                        <div className="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
-                        <div className="back">
-                            <h2>Glyph Profiles</h2>
-                            <p className="author">by <a href="http://google.com">Google Link?</a></p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p className="date">Feb 18, 2013</p>
-                        </div>
-                    </li>
-
-                    {/* <li id="p1" class="flipper">
-                        <div class="front"><img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/></div>
-                        <div class="back">
-                            <h2>Glyph Profiles</h2>
-                            <p class="author">by <a href="http://google.com">Google Link?</a></p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p>Diff P tag: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p class="date">Feb 18, 2013</p>
-                        </div>
-                    </li> */}
+                    <Card image={<img className='fullscreenImg' src={require('./images/standup/landing.jpeg')}/>} title={'Landing Page'} date={'Feb 18, 2013'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                    <Card image={<img className='fullscreenImg' src={require('./images/standup/dash.jpeg')}/>} title={'Dashboard'} date={'Sept 26, 2016'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'} />
                 </ul>
                 
+            </div>
+        )
+    }
+}
+
+export class Card extends Component {
+    state = {
+        active: true,
+    };
+    render() {
+        return (
+            <div id='appPreviewLayout'>
+                    <li id="p1" className="flipper">
+                        <div className={this.state.active ? 'display' : null} onClick={ () => this.setState({active: !this.state.active}) } >{this.props.image}</div>
+                        <div className={this.state.active ? 'text' : null} onClick={ () => this.setState({active: !this.state.active}) }>
+                            <h2>{this.props.title}</h2>
+                            {/* <p className="author">by <a href="http://google.com">Google Link?</a></p> */}
+                            <p>{this.props.description}</p>
+                            <p className="date">{this.props.date}</p>
+                        </div>
+                    </li>
             </div>
         )
     }
